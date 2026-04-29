9dd3b9fef6810c5d258584cb06d2c2cc3e0e97dd
nicolashacku <nicolasjimenez089@gmail.com>
2026-04-28 23:03:28 -0500

    fix white page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lamiTrader/',
+  base: '/', // En Vercel DEBE ser '/'
   server: {
     port: 5173,
     proxy: {
